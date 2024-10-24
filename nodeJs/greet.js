1d0533079a71ccc197fa0c9e60afde0e3680109e
lastBcell <saahil.sl.arnd@gmail.com>
2024-10-24 12:47:24 +0530

    node sever

---
+++ nodeJs/greet.js
@@ -0,0 +1,10 @@
+// Define the function
+sayHello ={
+     a1:"hello",
+     a2:"hai",
+    a3:function(){
+        return sayHello.a1 + sayHello.a2
+    }
+}
+  module.exports = sayHello;
+  
---
+++ nodeJs/import.js
@@ -0,0 +1,6 @@
+// Require the function from greetings.js
+const say = require('./greet.js');
+
+// Call the function
+const greetingMessage = say.a3();
+console.log(greetingMessage); // Outputs: Hello, John!
